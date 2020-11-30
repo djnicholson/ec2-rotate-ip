e923cf42ba3ee311bbcad4cbcdd5eff8cd52e3dc
David Nicholson <me@djnicholson.com>
2020-11-29 18:50:39 -0800

    implementation

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-Script to automatically rotate an Elastic IP from within an EC2 instance.
+# ec2-rotate-ip
+
+This is a script that can be run periodically to change the public-facing IP on one or more EC2 instances.
+
+## Pre-requisites
+
+Ensure the following pre-requisites before running the script.
+
+### Instances
+
+For any instance that should have its public IP rotated:
+
+1. Associate an Elastic IP with that instance.
+2. Create a tag on the Elastic IP:
+   - **rotate=1** _(key="rotate", value="1")_
+
+### Environment
+
+You will need Node.js installed and your environment configured with credentials to use the AWS Node.js SDK.
+
+#### Required AWS permissions:
+
+- `ec2:ReleaseAddress`
+- `ec2:DescribeAddresses`
+- `ec2:CreateTags`
+- `ec2:AssociateAddress`
+- `ec2:AllocateAddress`
+
+#### Example IAM policy:
+
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:ReleaseAddress",
+                "ec2:DescribeAddresses",
+                "ec2:CreateTags",
+                "ec2:AssociateAddress",
+                "ec2:AllocateAddress"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+```
+
+#### Environment configuration instructions:
+
+https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-node.html
+
+## Instructions
+
+Run the following command whenever you want to rotate IPs:
+
+```
+npx ec2-rotate-ip
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,100 @@
-console.log("Hello world");
+import * as aws from "aws-sdk";
+
+const TagRotate = "rotate";
+const TagOne = "1";
+
+const ec2 = new aws.EC2();
+
+const addTag = (resourceId: string, tag: string, value: string) =>
+  ec2
+    .createTags({
+      Resources: [resourceId],
+      Tags: [{ Key: tag, Value: value }],
+    })
+    .promise();
+
+const getAddresses = async () =>
+  (await ec2.describeAddresses().promise()).Addresses;
+
+const main = async () => {
+  let addresses = await getAddresses();
+  if (!addresses) {
+    console.error("describeAddresses API did not provide an address list");
+    return;
+  }
+
+  const addressesToRotate = addresses.filter(
+    (_) =>
+      !!_.AssociationId &&
+      !!_.InstanceId &&
+      !!_.PrivateIpAddress &&
+      _.Tags?.find((_) => _.Key === TagRotate && _.Value === TagOne)
+  );
+  if (!addressesToRotate.length) {
+    console.warn(
+      "Could not find any IPs with tag rotate=1 that are attached to an EC2 instance"
+    );
+  }
+
+  for (const addressToRotate of addressesToRotate) {
+    const instanceId = addressToRotate.InstanceId;
+    const previousPublicIp = addressToRotate.PublicIp;
+    const privateIp = addressToRotate.PrivateIpAddress;
+    const previousAssociation = addressToRotate.AssociationId;
+    console.log(instanceId, `Current IP: ${previousPublicIp} -> ${privateIp}`);
+    console.log(instanceId, `Association ID: ${previousAssociation}`);
+    try {
+      const allocationResult = await ec2.allocateAddress().promise();
+      const newAllocationId = allocationResult.AllocationId;
+      if (!newAllocationId) {
+        console.error(instanceId, "Failed to allocate new public IP");
+      } else {
+        await addTag(newAllocationId, TagRotate, TagOne);
+        console.log(
+          instanceId,
+          `Associating: ${allocationResult.PublicIp} -> ${privateIp}`
+        );
+        await ec2
+          .associateAddress({
+            AllocationId: newAllocationId,
+            AllowReassociation: true,
+            InstanceId: instanceId,
+            PrivateIpAddress: privateIp,
+          })
+          .promise();
+      }
+    } catch (e) {
+      console.error(instanceId, "Unexpected error", e.message);
+    }
+  }
+
+  addresses = await getAddresses();
+  if (!addresses) {
+    console.warn("Skipping cleanup");
+    return;
+  }
+
+  const toClean = addresses.filter(
+    (_) =>
+      !_.AssociationId &&
+      !_.InstanceId &&
+      !_.PrivateIpAddress &&
+      _.Tags?.find((_) => _.Key === TagRotate && _.Value === TagOne)
+  );
+  for (const addressToRelease of toClean) {
+    try {
+      console.log(
+        `Releasing unused public IP: ${addressToRelease.PublicIp} (${addressToRelease.AllocationId})`
+      );
+      await ec2
+        .releaseAddress({ AllocationId: addressToRelease.AllocationId })
+        .promise();
+    } catch (e) {
+      console.error(
+        `Could not release unused address ${addressToRelease.PublicIp} (${addressToRelease.AllocationId}): ${e.message}`
+      );
+    }
+  }
+};
+
+main();
